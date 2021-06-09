5bb2eddc3984755e36519b046a8c27b9558eaa1a
Ameer Hamza <ameer.hamza1811@gmail.com>
2021-06-09 23:16:54 +0500

    Add material-ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-
 import './App.css';
 import React, { useState } from 'react';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
 
 function App() {
 	const [ todos, setTodos ] = useState([ 'a', 'b' ]);
-	const [input, setInput] = useState('')
+	const [ input, setInput ] = useState('');
 
-	const addTodo = (event) =>{
-		console.log("asdf");
-		setTodos([...todos, input])
-	}
+	const addTodo = (event) => {
+		//	form submit par ye function chale ga
+
+		event.preventDefault(); // stop refresh the page on click
+
+		console.log('asdf');
+		setTodos([ ...todos, input ]);
+
+		setInput(''); // value submit k bad input empty kar dega
+	};
 
 	return (
 		<div className="App">
-			<h1>Hello</h1>
-			<input value={input} onChange={event=>setInput(event.target.value)}/>
-			<button onClick={addTodo}>Add Todo</button>
+			<form>
+				<h1>Hello</h1>
+				{/* <input /> */}
+				{/* <button type="submit" onClick={addTodo}>
+					Add Todo
+				</button> */}
 
+				<FormControl>
+					<InputLabel>Write a Todo</InputLabel>
+					<Input value={input} onChange={(event) => setInput(event.target.value)}/>
+					
+				</FormControl>
 
+				<Button variant="contained" color="primary" type="submit" onClick={addTodo} disabled={!input}>
+					Add Todo
+				</Button>
+			</form>
 
 			<ul>
 				{todos.map((todo) => {
