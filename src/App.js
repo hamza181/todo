288dfc4173a9fcfa7b599921d48d7f1b62b5f4f8
Ameer Hamza <ameer.hamza1811@gmail.com>
2021-06-10 01:15:48 +0500

    Add and receive data from firebase

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
 import Todo from './Todo';
+import {db} from './firebase'
+import firebase from 'firebase'
 
 function App() {
-	const [ todos, setTodos ] = useState([ 'a', 'b' ]);
+	const [ todos, setTodos ] = useState([]);
 	const [ input, setInput ] = useState('');
+
+	useEffect(() => {
+		// jab bhi app reload hogi ye 1 bar dubara chale ga
+
+		db.collection('todos').onSnapshot(snapshot =>{
+			setTodos(snapshot.docs.map(doc =>doc.data().todo))
+		})
+	},
+	// array is empty it means har rerender par use effect ka code chale ga
+	// agar niche array ma agar koi variable name dal den to sirf us variable k change par rerender hoga 
+	[])
 
 	const addTodo = (event) => {
 		//	form submit par ye function chale ga
 
 		event.preventDefault(); // stop refresh the page on click
 
-		console.log('asdf');
-		setTodos([ ...todos, input ]);
+		db.collection('todos').add({
+			todo: input,
+			timestamp: firebase.firestore.FieldValue.serverTimestamp() 	// sort todos according to firebase server time
+		})
+
+		// setTodos([ ...todos, input ]);
 
 		setInput(''); // value submit k bad input empty kar dega
 	};
@@ -21,7 +38,7 @@ function App() {
 	return (
 		<div className="App">
 			<form>
-				<h1>Hello</h1>
+				<h1>Todo App</h1>
 				{/* <input /> */}
 				{/* <button type="submit" onClick={addTodo}>
 					Add Todo
